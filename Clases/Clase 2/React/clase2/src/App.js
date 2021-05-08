3830f7bd70ed834c34773729066f5fe6109fcce3
Luis Cascante <luisalonzocascante@gmail.com>
2021-05-08 08:25:19 -0600

    clase 3

--- Clases/Clase 2/React/clase2/src/App.js
+++ Clases/Clase 2/React/clase2/src/App.js
@@ -5,19 +5,20 @@ import ComponenteFuncional from "./components/ComponenteFuncional";
 import ListaPersonasClase from "./components/ListaPersonasClase";
 import ListaPersonasFuncion from "./components/ListaPersonasFuncion";
 
+const arregloPersonas = [
+  { nombre: "Luis", edad: 204, perros: 3 },
+  { nombre: "Irene", edad: 204, perros: 2 },
+  { nombre: "Cristian", edad: 204, perros: 4 },
+  { nombre: "Daniel", edad: 204, perros: 6 },
+  { nombre: "Diego", edad: 204, perros: 7 },
+  { nombre: "Felipe", edad: 204, perros: 0 },
+  { nombre: "Hector", edad: 204, perros: 1 },
+];
+
 function App() {
   const [contenidoClase, setContenidoClase] = useState("Propiedad");
   const [contenidoFuncion, setContenidoFuncion] = useState("Propiedad");
-
-  const personas = [
-    { nombre: "Luis", edad: 204, perros: 3 },
-    { nombre: "Irene", edad: 204, perros: 2 },
-    { nombre: "Cristian", edad: 204, perros: 4 },
-    { nombre: "Daniel", edad: 204, perros: 6 },
-    { nombre: "Diego", edad: 204, perros: 7 },
-    { nombre: "Felipe", edad: 204, perros: 0 },
-    { nombre: "Hector", edad: 204, perros: 1 },
-  ];
+  const [personas, setPersonas] = useState(arregloPersonas);
 
   return (
     <>
@@ -32,7 +33,7 @@ function App() {
         />
       </div>
       <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <ListaPersonasClase personas={personas} />
+        <ListaPersonasClase personas={personas} setPersonas={setPersonas} />
         <ListaPersonasFuncion personas={personas} />
       </div>
     </>
--- Clases/Clase 2/React/clase2/src/components/ListaPersonasClase.js
+++ Clases/Clase 2/React/clase2/src/components/ListaPersonasClase.js
@@ -5,7 +5,26 @@ import PropTypes from "prop-types";
 export default class ListaPersonasClase extends Component {
   static propTypes = {
     personas: PropTypes.array,
+    setPersonas: PropTypes.func,
   };
+
+  agregarPersona() {
+    this.props.setPersonas([
+      ...this.props.personas,
+      { nombre: "Jody", edad: 204, perros: 5 },
+    ]);
+  }
+
+  modificarPersona() {
+    this.props.personas[0] = { nombre: "Luis", edad: 205, perros: 7 };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.agregarPersona();
+      this.modificarPersona();
+    }, 3000);
+  }
 
   render() {
     return (
