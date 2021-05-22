bf8b48a8c57b46b53e239b141b86dd6bcdd5547e
Luis Cascante <luiscascante@Luiss-MacBook-Pro.local>
2021-05-22 10:21:00 -0600

    algo

--- Clases/Clase4/Tarea1/src/components/AlbumContainer/AlbumContainer.tsx
+++ Clases/Clase4/Tarea1/src/components/AlbumContainer/AlbumContainer.tsx
@@ -1,20 +1,53 @@
+import axios from 'axios';
 import React, {useEffect, useState} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, ActivityIndicator} from 'react-native';
 import IAlbum from '../../models/Album';
 import ListaAlbum from '../ListaAlbum';
 
 const AlbumContainer = () => {
   const [albums, setAlbums] = useState<IAlbum[] | null>(null);
-  // const [cargando, setCargando] = useState(false);
+  const [photos, setPhotos] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const cargarAlbums = async () => {
     try {
-      const data = await fetch('https://jsonplaceholder.typicode.com/albums');
-      const dataAlbums = await data.json();
-      setAlbums(dataAlbums);
+      const dataAlbums = await fetch(
+        'https://jsonplaceholder.typicode.com/albums',
+      );
+      const dataAlbumsJSON = await dataAlbums.json();
+
+      const dataPhotos = await fetch(
+        'https://jsonplaceholder.typicode.com/photos',
+      );
+      const dataPhotosJSON = await dataPhotos.json();
+
+      setAlbums(dataAlbumsJSON);
+      setPhotos(dataPhotosJSON);
+
+      setLoading(false);
     } catch (error) {
       console.error(error);
     }
+  };
+
+  const cargarAlbumsPromise = () => {
+    fetch('https://jsonplaceholder.typicode.com/albums')
+      .then(data => {
+        data
+          .json()
+          .then(dataAlbums => setAlbums(dataAlbums))
+          .catch(error => console.error(error));
+      })
+      .catch(error => console.error(error));
+
+    fetch('https://jsonplaceholder.typicode.com/photos')
+      .then(data => {
+        data
+          .json()
+          .then(dataAlbums => setAlbums(dataAlbums))
+          .catch(error => console.error(error));
+      })
+      .catch(error => console.error(error));
   };
 
   // Es como llamar componentDidMount
@@ -28,7 +61,8 @@ const AlbumContainer = () => {
 
   return (
     <View>
-      {albums ? <ListaAlbum albums={albums} /> : <Text>Cargando</Text>}
+      {/* {albums ? <ListaAlbum albums={albums} /> : <Text>Cargando</Text>} */}
+      {loading ? <ActivityIndicator /> : <Text>Listo</Text>}
     </View>
   );
 };
