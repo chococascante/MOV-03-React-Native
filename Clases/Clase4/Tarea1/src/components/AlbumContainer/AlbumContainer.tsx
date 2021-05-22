e43d534f561f8d256a2f466ff20c93d7ad37389c
Luis Cascante <luisalonzocascante@gmail.com>
2021-05-22 08:51:26 -0600

    llamado asincrono

---
+++ Clases/Clase4/Tarea1/src/components/AlbumContainer/AlbumContainer.tsx
@@ -0,0 +1,34 @@
+import React, {useEffect, useState} from 'react';
+import {View, Text} from 'react-native';
+
+const AlbumContainer = () => {
+  const [albums, setAlbums] = useState(null);
+  const [cargando, setCargando] = useState(false);
+
+  const cargarAlbums = async () => {
+    try {
+      const data = await fetch('https://jsonplaceholder.typicode.com/albums');
+      console.log(data);
+      // setAlbums(data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // Es como llamar componentDidMount
+  useEffect(() => {
+    async function llamarCargarAlbums() {
+      await cargarAlbums();
+    }
+
+    llamarCargarAlbums();
+  }, []);
+
+  return (
+    <View>
+      <Text>Hola</Text>
+    </View>
+  );
+};
+
+export default AlbumContainer;
